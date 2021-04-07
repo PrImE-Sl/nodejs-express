d76a964a51e48a3d753b8ab2e3ea499b24b4dc81
Viacheslav <slava9678@gmail.com>
2021-04-07 15:16:39 +0300

    Add files via upload

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MONGODB_URI: 'mongodb+srv://slavik:3GEQ0QVoahBRGI9c@cluster0.axaij.mongodb.net/shop',
+	SESSION_SECRET: 'some secret value',
+	SENDGRID_API_KEY: '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',
+	EMAIL_FROM: 'nodejs@email.ru',
+	BASE_URL: 'http://localhost:3000'
+}
